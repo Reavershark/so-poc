18f46de16476fe666b13becad3b2e949629b520f
Reavershark <jonasty1999@gmail.com>
2020-03-09 16:37:08 +0100

    Add links to other services

--- so-manager/react/src/pages/Home.js
+++ so-manager/react/src/pages/Home.js
@@ -1,25 +1,33 @@
 import React from "react";
 
-import Iframe from "react-iframe";
-import { Link } from '@material-ui/core';
+import { Typography, Link, Card, CardHeader, CardContent, CardActions, Grid } from "@material-ui/core";
+
+const services = [
+  { name: "Security Onion", url: "https://so-sopoc.duckdns.org" },
+  { name: "CyberChef", url: "https://so-sopoc.duckdns.org/cyberchef/cyberchef.htm" },
+  { name: "Squert", url: "https://so-sopoc.duckdns.org/squert/" },
+  { name: "Ntopng", url: "https://ntopng-sopoc.duckdns.org/" },
+  { name: "TheHive", url: "https://thehive-sopoc.duckdns.org" },
+  { name: "Cortex", url: "https://cortex-sopoc.duckdns.org" }
+];
 
 function Home() {
   return (
     <React.Fragment>
       <h1>Welcome</h1>
-      <Link href="https://so-sopoc.duckdns.org/app/kibana">
-        Kibana
-      </Link>
-      <Iframe
-        url="/so/app/kibana"
-        width="100%"
-        height="600px"
-        id="myId"
-        className="myClassname"
-        display="initial"
-        position="relative"
-        style={{ padding: 20 }}
-      />
+      {services.map(service => (
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            <Card>
+              <CardContent>
+                <Typography>
+                  <Link href={service.url}>{service.name}</Link>
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      ))}
     </React.Fragment>
   );
 }
