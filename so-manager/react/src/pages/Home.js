11104e2d6ff4f64cff8539660953d6927ff33306
Reavershark <jonasty1999@gmail.com>
2020-03-10 16:03:21 +0100

    Homepage with cards

--- so-manager/react/src/pages/Home.js
+++ so-manager/react/src/pages/Home.js
@@ -1,32 +1,108 @@
 import React from "react";
 
-import { Typography, Link, Card, CardHeader, CardContent, CardActions, Grid } from "@material-ui/core";
+import {
+  Typography,
+  Link,
+  Card,
+  CardHeader,
+  CardContent,
+  CardActions,
+  CardActionArea,
+  Grid
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  card: {
+    width: 200,
+    minHeight: 100,
+    marginRight: 20
+  },
+  actionArea: {
+    height: "100%"
+  }
+});
 
 const services = [
-  { name: "Security Onion", url: "https://so-sopoc.duckdns.org" },
-  { name: "CyberChef", url: "https://so-sopoc.duckdns.org/cyberchef/cyberchef.htm" },
-  { name: "Squert", url: "https://so-sopoc.duckdns.org/squert/" },
-  { name: "Ntopng", url: "https://ntopng-sopoc.duckdns.org/" },
-  { name: "TheHive", url: "https://thehive-sopoc.duckdns.org" },
-  { name: "Cortex", url: "https://cortex-sopoc.duckdns.org" }
+  {
+    category: "Security Onion",
+    items: [
+      {
+        name: "Security Onion",
+        description: "Security Onion homepage",
+        url: "https://so-sopoc.duckdns.org"
+      },
+      {
+        name: "Kibana",
+        description: "NIDS, HIDS and Zeek log visualization",
+        url: "https://so-sopoc.duckdns.org/app/kibana"
+      },
+      {
+        name: "Squert",
+        description: "IDS alert viewer",
+        url: "https://so-sopoc.duckdns.org/squert/"
+      },
+      {
+        name: "CyberChef",
+        description: "Cyber Swiss Army Knife",
+        url: "https://so-sopoc.duckdns.org/cyberchef/cyberchef.htm"
+      }
+    ]
+  },
+  {
+    category: "TheHive",
+    items: [
+      {
+        name: "TheHive",
+        description: "Security incident response platform",
+        url: "https://thehive-sopoc.duckdns.org"
+      },
+      {
+        name: "Cortex",
+        description: "Analysis and response engine",
+        url: "https://cortex-sopoc.duckdns.org"
+      }
+    ]
+  },
+  {
+    category: "Ntop",
+    items: [
+      {
+        name: "Ntopng",
+        description: "Complete traffic monitor",
+        url: "https://ntopng-sopoc.duckdns.org/"
+      }
+    ]
+  }
 ];
 
 function Home() {
+  const classes = useStyles();
+
   return (
     <React.Fragment>
       <h1>Welcome</h1>
+
       {services.map(service => (
-        <Grid container spacing={3}>
-          <Grid item xs={6}>
-            <Card>
-              <CardContent>
-                <Typography>
-                  <Link href={service.url}>{service.name}</Link>
-                </Typography>
-              </CardContent>
-            </Card>
+        <React.Fragment>
+          <h2>{service.category}</h2>
+          <Grid container spacing={1}>
+            {service.items.map(item => (
+              <Card className={classes.card}>
+                <CardActionArea className={classes.actionArea} href={item.url}>
+                  <CardContent>
+                    <Typography align="center" variant="h6">
+                      {item.name}
+                    </Typography>
+                    <Typography align="center" variant="body2">
+                      {item.description}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            ))}
           </Grid>
-        </Grid>
+        </React.Fragment>
       ))}
     </React.Fragment>
   );
