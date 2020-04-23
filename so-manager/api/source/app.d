8bf80a4d720252b4304de27d8fb3938a75877ceb
Reavershark <jonasty1999@gmail.com>
2020-04-23 11:46:05 +0200

    Ntop route

--- so-manager/api/source/app.d
+++ so-manager/api/source/app.d
@@ -1,6 +1,49 @@
-import std.stdio;
+import vibe.appmain;
+import vibe.core.core;
+import vibe.core.log;
+import vibe.data.json;
+import vibe.http.router;
+import vibe.http.server;
+import vibe.http.client;
+import vibe.web.rest;
+import vibe.stream.operations;
+
+import core.thread;
+
+import std.exception;
+
+@safe:
+
+@path("/api/")
+interface RestAPISpec
+{
+  @path("/host/:ip")
+  string getHost(string _ip);
+}
+
+class RestAPI : RestAPISpec
+{
+  override string getHost(string _ip)
+  {
+    string result;
+
+    //--cookie user=admin; password=admin
+    requestHTTP("http://ntop-proxy/lua/rest/get/host/data.lua?host=" ~ _ip, (scope req) {
+    }, (scope res) {
+      enforce(res.headers["content-type"] == "application/json");
+      result = res.bodyReader.readAllUTF8();
+    });
+
+    return result;
+  }
+}
 
 shared static this()
 {
-	writeln("Edit source/app.d to start your project.");
+  Thread.getThis().name = "http";
+
+  auto router = new URLRouter;
+  router.registerRestInterface(new RestAPI);
+  auto settings = new HTTPServerSettings("127.0.0.1:8080");
+  listenHTTP(settings, router);
 }
